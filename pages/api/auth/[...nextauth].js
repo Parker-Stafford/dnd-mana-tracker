3f4f92d55e80f4a75cee26891a87aee719d2a8fd
Parker Stafford <parker.stafford92@gmail.com>
2020-12-22 01:48:10 -0800

    Add callback for redirect on sign in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,10 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 const options = {
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+  },
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -12,7 +16,13 @@ const options = {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
-  database: process.env.DB_URI,
+  callbacks: {
+    redirect: async (url, baseUrl) => {
+      console.log(url, baseUrl);
+      return Promise.resolve(baseUrl);
+    },
+  },
+  // database: process.env.DB_URI,
 };
 
 export default (req, res) => NextAuth(req, res, options);
