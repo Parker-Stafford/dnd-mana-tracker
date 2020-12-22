5d9bab4e1eee594215db4ae9a5d8933e971e1166
Parker Stafford <parker.stafford92@gmail.com>
2020-12-21 16:27:47 -0800

    Add next-auth configuration for google and facebook signin.

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default (req, res) => NextAuth(req, res, options);
