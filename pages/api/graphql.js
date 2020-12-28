5690947a5f3f625a20b8655cc28f28e6cae69d91
Parker Stafford <parker.stafford92@gmail.com>
2020-12-28 11:56:41 -0800

    Implement gql api for character querys

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,14 +1,32 @@
 import { gql, ApolloServer } from 'apollo-server-micro';
+import prisma from '../../database/config';
 
 const typeDefs = gql`
   type Query {
-    hello: String!
+    characters(user_id: ID!): [Character!]
+  }
+
+  type Character {
+    id: ID!
+    name: String!
+    current_mana: Int!
+    max_mana: Int!
+    photo_url: String
+    level: Int!
+    mana_pots: Int
+    greater_pots: Int
+    user_id: Int!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello!',
+    characters: (_parent, args, _context) => (
+      prisma.characters.findMany({
+        where: { user_id: +args.user_id },
+        orderBy: { id: 'asc' },
+      })
+    ),
   },
 };
 
