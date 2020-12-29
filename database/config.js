dea6340704b17318af8f489a85cdc2cdde8c856f
Parker Stafford <parker.stafford92@gmail.com>
2020-12-29 15:25:31 -0800

    Modify prisma client in development to use same client every time. Hot reloading generating too many clients

--- database/config.js
+++ database/config.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// eslint-disable-next-line import/no-mutable-exports
+let prisma = null;
+
+// Stop hot-reloading from generating too many prisma clients in dev
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  global.prisma = global.prisma || new PrismaClient();
+  prisma = global.prisma;
+}
 
 export default prisma;
