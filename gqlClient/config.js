60a21f01af3321eced889f1fd0acefd5aabe6491
Parker Stafford <parker.stafford92@gmail.com>
2020-12-28 20:12:36 -0800

    Fix connection uri.

--- gqlClient/config.js
+++ gqlClient/config.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `${process.env.NEXTAUTH_URL}/api/graphql`,
+  uri: 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
