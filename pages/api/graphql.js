34e323730a43291e78a4389026371786268e5619
Parker Stafford <parker.stafford92@gmail.com>
2020-12-28 17:56:34 -0800

    Implement createCharacter mutation

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,9 +1,23 @@
+/* eslint-disable camelcase */
 import { gql, ApolloServer } from 'apollo-server-micro';
 import prisma from '../../database/config';
 
 const typeDefs = gql`
   type Query {
     characters(user_id: ID!): [Character!]
+  }
+
+  type Mutation {
+    createCharacter(
+      name: String,
+      current_mana: Int,
+      max_mana: Int,
+      photo_url: String,
+      level: Int,
+      mana_pots: Int,
+      greater_pots: Int,
+      user_id: Int
+    ): Character
   }
 
   type Character {
@@ -27,6 +41,22 @@ const resolvers = {
         orderBy: { id: 'asc' },
       })
     ),
+  },
+  Mutation: {
+    createCharacter(_parent, data) {
+      const id = data.user_id;
+      const query = { data: {} };
+      const keys = Object.keys(data);
+      for (let i = 0; i < keys.length; i++) {
+        if (keys[i] !== 'user_id') {
+          query.data[keys[i]] = data[keys[i]];
+        } else {
+          query.data.users = { connect: { id } };
+        }
+      }
+      query.data.users.connect = { id };
+      return prisma.characters.create(query);
+    },
   },
 };
 
