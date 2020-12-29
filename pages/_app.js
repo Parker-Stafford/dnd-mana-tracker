9c88faea8be1ed83ae54492da79b5beadc903989
Parker Stafford <parker.stafford92@gmail.com>
2020-12-28 20:13:11 -0800

    Wrap page in apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import '../styles/globals.css';
 import { Provider } from 'next-auth/client';
+import { ApolloProvider } from '@apollo/client';
+import client from '../gqlClient/config';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </ApolloProvider>
   );
 }
 
