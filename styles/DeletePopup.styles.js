9e64820b899cb6b7de2f03fffaf496eb515573f3
Parker Stafford <parker.stafford92@gmail.com>
2020-12-30 11:25:49 -0800

    Implement DeletePopup component

---
+++ styles/DeletePopup.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 3;
+  background-color: rgba(0, 0, 0, .4);
+`;
+
+export const Popup = styled.div`
+position: fixed;
+top: 50%;
+left: 50%;
+z-index: 3;
+width: 50%;
+background-color: white;
+transform: translate(-50%, -50%);
+border-radius: 12px;
+box-sizing: border-box;
+`;
