6f19b1f4491ea13973b85da2106018d9d157b08a
Parker Stafford <parker.stafford92@gmail.com>
2020-12-30 15:22:22 -0800

    Fix overlay from wrapping modal

--- components/DeletePopup.js
+++ components/DeletePopup.js
@@ -16,14 +16,13 @@ export default function DeletePopup({
 
   return ReactDOM.createPortal(
     <>
-      <Overlay onClick={closeFunc}>
-        <Popup>
-          Are you sure you want to delete {name}? <br />
-          This action can&apos;t be undone.
-          <button type="button" onClick={delAndClose}>Yes</button>
-          <button type="button" onClick={closeFunc}>No</button>
-        </Popup>
-      </Overlay>
+      <Overlay onClick={closeFunc} />
+      <Popup>
+        Are you sure you want to delete {name}? <br />
+        This action can&apos;t be undone.
+        <button type="button" onClick={delAndClose}>Yes</button>
+        <button type="button" onClick={closeFunc}>No</button>
+      </Popup>
     </>,
     document.getElementById('portal'),
   );
