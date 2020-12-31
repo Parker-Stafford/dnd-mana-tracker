b5b1b7249d9f16d3f20b4a2d78c71e3b19c42f5d
Parker Stafford <parker.stafford92@gmail.com>
2020-12-30 19:50:06 -0800

    Refactor to be component instead of popup modal

---
+++ components/SpellCaster.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import spellMap from '../helpers/spellManaMap';
+
+export default function SpellCaster({ castFunc }) {
+  const [spellLevel, setSpellLevel] = useState('cantrip');
+
+  function changeLevel(event) {
+    setSpellLevel(event.target.value);
+  }
+  async function cast(event) {
+    event.preventDefault();
+    const cost = spellMap[spellLevel];
+    await castFunc(cost);
+  }
+
+  return (
+    <form>
+      <label htmlFor="spellLevel">Spell level:
+        <select onChange={changeLevel} id="spellLevel">
+          <option value="cantrip">Cantrip</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
+        </select>
+      </label>
+      <button type="submit" onClick={cast}>Cast!</button>
+    </form>
+  );
+}
