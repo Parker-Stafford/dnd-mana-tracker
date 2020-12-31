f508d8be7d7219730be73b738137d1c63500f4ec
Parker Stafford <parker.stafford92@gmail.com>
2020-12-30 17:18:19 -0800

    Add spell casting and potion drinking functionality

--- pages/character/[id].js
+++ pages/character/[id].js
@@ -9,15 +9,17 @@ import SignIn from '../../components/SignIn';
 import DeleteMessage from '../../components/DeleteMessage';
 import DeletePopup from '../../components/DeletePopup';
 import UpdatePopup from '../../components/UpdatePopup';
+import SpellPopup from '../../components/SpellPopup';
 import { CharImg } from '../../styles/characters.styles.js';
 
-export default function Character({ initialCharacter }) {
+export default function Character({ initialCharacter, userId }) {
   const [session, loading] = useSession();
   const [char, setChar] = useState(initialCharacter);
   const [showDelete, setShowDelete] = useState(false);
   const [showUpdate, setShowUpdate] = useState(false);
+  const [showSpell, setShowSpell] = useState(false);
   const [deleteMutation, { data: deleteData, error: deleteError }] = useMutation(DELETE_CHARACTER);
-  const [upsertChar, { data: upsertData, error: upsertError }] = useMutation(UPSERT_CHARACTER);
+  const [upsertChar, { error: upsertError }] = useMutation(UPSERT_CHARACTER);
 
   async function deleteChar() {
     await deleteMutation({ variables: { id: char.id } });
@@ -26,25 +28,65 @@ export default function Character({ initialCharacter }) {
   function openUpdate() {
     setShowUpdate(true);
   }
-
   function closeUpdate() {
     setShowUpdate(false);
   }
-
   function openDelete() {
     setShowDelete(true);
   }
-
   function closeDelete() {
     setShowDelete(false);
   }
+  function openSpell() {
+    setShowSpell(true);
+  }
+  function closeSpell() {
+    setShowSpell(false);
+  }
 
   async function updateChar(update) {
-    console.log('test');
     const { data } = await upsertChar(update);
     setChar(data.upsertCharacter);
-    console.log(upsertData);
     closeUpdate();
+  }
+
+  async function drinkPotion(event) {
+    const potionType = event.target.id;
+    const update = { variables: { id: char.id, user_id: userId, maxMana: char.max_mana } };
+    if (potionType === 'manaPots') {
+      update.variables[potionType] = char.mana_pots - 1;
+      update.variables.currentMana = Math.min(char.current_mana + 10, char.max_mana);
+    } else {
+      update.variables[potionType] = char.greater_pots - 1;
+      update.variables.currentMana = Math.min(char.current_mana + 20, char.max_mana);
+    }
+    const { data } = await upsertChar(update);
+    setChar(data.upsertCharacter);
+  }
+
+  async function takeRest(event) {
+    const restType = event.target.id;
+    const update = { variables: { id: char.id, user_id: userId, maxMana: char.max_mana } };
+    if (restType === 'shortRest') {
+      update.variables.currentMana = Math.min(char.current_mana + 10, char.max_mana);
+    } else {
+      update.variables.currentMana = char.max_mana;
+    }
+    const { data } = await upsertChar(update);
+    setChar(data.upsertCharacter);
+  }
+
+  async function castSpell(cost) {
+    const update = {
+      variables: {
+        id: char.id,
+        user_id: userId,
+        maxMana: char.max_mana,
+        currentMana: char.current_mana - cost,
+      },
+    };
+    const { data } = await upsertChar(update);
+    setChar(data.upsertCharacter);
   }
 
   return (
@@ -69,8 +111,12 @@ export default function Character({ initialCharacter }) {
               <CharImg src={char.photo_url || 'https://i.imgur.com/VKYcZgy.png'} alt="Character" />
               <div>{char.level}</div>
               <div>Mana: {char.current_mana}/{char.max_mana}</div>
-              <div>Mana pots: {char.mana_pots}</div>
-              <div>Greater mana pots: {char.greater_pots}</div>
+              <button id="manaPots" type="button" disabled={!char.mana_pots} onClick={drinkPotion}>Drink mana potion {char.mana_pots}</button>
+              <button id="greaterPots" type="button" disabled={!char.greater_pots} onClick={drinkPotion}>Drink greater mana potion {char.greater_pots}</button><br />
+              <button id="shortRest" type="button" onClick={takeRest}>Short rest</button>
+              <button id="longRest" type="button" onClick={takeRest}>Long rest</button>
+              <button id="castSpell" type="button" onClick={openSpell}>Cast spell!</button>
+              <br />
               <button type="button" onClick={openDelete}>Delete</button>
               <button type="button" onClick={openUpdate}>Edit</button>
               <button type="button" onClick={() => { signOut({ callbackUrl: `${process.env.NEXTAUTH_URL}` }); }}>Sign out</button>
@@ -102,6 +148,11 @@ export default function Character({ initialCharacter }) {
             updateFunc={updateChar}
             userId={session.user.id}
           />
+          <SpellPopup
+            showing={showSpell}
+            closeFunc={closeSpell}
+            castFunc={castSpell}
+          />
         </>
       )}
     </>
@@ -112,13 +163,15 @@ export async function getServerSideProps(context) {
   const session = await getSession(context);
   const { id } = context.query;
   let initialCharacter = null;
+  let userId = null;
   if (session) {
+    userId = session.user.id;
     const client = initializeApollo();
     const result = await GET_CHARACTER(id, client);
     initialCharacter = result.data.character;
   }
   return {
     props:
-    { initialCharacter },
+    { initialCharacter, userId },
   };
 }
