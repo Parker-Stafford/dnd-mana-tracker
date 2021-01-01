db4228e45b91f6faa69657625b63365d51fad7a8
Parker Stafford <parker.stafford92@gmail.com>
2021-01-01 14:20:33 -0800

    Add signin buttons for google and facebook

---
+++ pages/auth/providers.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { providers, signIn } from 'next-auth/client';
+import GoogleIcon from '../../components/GoogleIcon';
+import {
+  FBButton,
+  FBIcon,
+  GButton,
+  GText,
+  ProvidersWrapper,
+  Title,
+  ProviderDiv,
+} from '../../styles/providers.styles';
+
+export default function SignIn({ provs }) {
+  return (
+    <>
+      <ProvidersWrapper>
+        <Title>Welcome to D&D Mana Tracker!</Title>
+        {Object.values(provs).map((provider) => (
+          <ProviderDiv key={provider.name}>
+            {provider.name === 'Google' && (
+              <GButton type="button" onClick={() => { signIn(provider.id, { callbackUrl: `${process.env.NEXTAUTH_URL}` }); }}>
+                <GoogleIcon />
+                <GText>Sign in with Google</GText>
+              </GButton>
+            )}
+            {provider.name === 'Facebook' && (
+              <FBButton type="button" onClick={() => { signIn(provider.id, { callbackUrl: `${process.env.NEXTAUTH_URL}` }); }}>
+                <FBIcon className="fab fa-facebook" />
+                <div>Sign in with Facebook</div>
+              </FBButton>
+            )}
+          </ProviderDiv>
+        ))}
+      </ProvidersWrapper>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      provs: await providers(),
+    },
+  };
+}
