fad70bd6a78ae18aff7b052e1d6adad55445ba43
Parker Stafford <parker.stafford92@gmail.com>
2020-12-31 18:16:45 -0800

    Add bootstrap, fontawesome, and google fonts cdn

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import '../styles/globals.css';
+import Head from 'next/head';
 import { Provider } from 'next-auth/client';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../apollo/config';
@@ -9,6 +9,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <Provider session={pageProps.session}>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossOrigin="anonymous"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+            integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <Component {...pageProps} />
         <div id="portal" />
       </Provider>
