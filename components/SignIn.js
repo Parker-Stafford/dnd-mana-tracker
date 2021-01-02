38bb63761338ca9c97cb1d7c4076d36e0d6fed50
Parker Stafford <parker.stafford92@gmail.com>
2021-01-02 14:55:52 -0800

    Style generic signin for protected pages

--- components/SignIn.js
+++ components/SignIn.js
@@ -9,7 +9,6 @@ export default function SignIn() {
       <SignInMessage>You&apos;re not signed in! Click the button below to sign in!</SignInMessage>
       <ButtonWrapper>
         <BlueButton type="button" onClick={signIn}>Sign in</BlueButton>
-
       </ButtonWrapper>
     </>
   );
