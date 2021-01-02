45455cbf216b89a1f5544792041b2ac4d6119125
Parker Stafford <parker.stafford92@gmail.com>
2021-01-01 17:55:47 -0800

    Style create-character page

---
+++ styles/create-character.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { ProvidersWrapper } from './providers.styles.js';
+
+export const FormWrapper = styled(ProvidersWrapper)`
+  background-color: #153e90;
+  height: 810px;
+  color: white;
+  width: 600px;
+  padding: 20px 0;
+  top: 57%;
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  width: 75%;
+  margin: 12px auto;
+  color: #153e90;
+`;
